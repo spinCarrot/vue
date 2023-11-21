11177b4b501fbc9f80bf2cce5a511f8985eaf442
spinCarrot <h27732197@gmail.com>
2023-11-21 17:47:17 +0800

    mmmm

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,17 +13,21 @@ export default{
 
 <template>
 <div class="headerShow">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/"></RouterLink>
+    <RouterLink to="/">首頁</RouterLink>
+    <RouterLink to="/Visit">觀光</RouterLink>
+    <RouterLink to="/Life">住宿</RouterLink>
+    <RouterLink to="/Transport">交通</RouterLink>
 
     
 </div>
+
 </template>
 
 <style scoped lang="scss">
-// .headerShow{
-//     width: 100%;
-//     height: 100%;
-//     background-color: aqua;
-// }
+.headerShow{
+    background-color: rgb(195, 230, 254);
+}
+*{
+    font-size: 35px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,29 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // },
-    
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/Visit',
+      name: 'visit',
+      component : ()=> import('../views/Visit.vue')
+      
+    },
+    {
+      path: '/Life',
+      name: 'life',
+      component : ()=> import('../views/Life.vue')
+      
+    },
+    {
+      path: '/Transport',
+      name: 'transport',
+      component : ()=> import('../views/Transport.vue')
+      
+    },
   ]
 })
 

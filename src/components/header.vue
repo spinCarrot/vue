2d6e6ab1e6716bc330fa42ff5f92cf3c1fc869ca
m200m24 <m200isarifle@gmail.com>
2023-11-21 15:25:15 +0800

    del

---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<script >
+import { RouterLink, RouterView } from 'vue-router'
+export default{
+    data(){
+        return{
+        }
+    },
+    components:{
+        RouterLink,
+    }
+}
+</script>
+
+<template>
+<div class="headerShow">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/"></RouterLink>
+
+    
+</div>
+</template>
+
+<style scoped lang="scss">
+// .headerShow{
+//     width: 100%;
+//     height: 100%;
+//     background-color: aqua;
+// }
+</style>

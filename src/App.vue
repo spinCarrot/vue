bfeb56ae14a75636aae86fb76077b295464ce077
spinCarrot <h27732197@gmail.com>
2023-11-22 09:38:00 +0800

    Revert "mmmm"
    This reverts commit 11177b4b501fbc9f80bf2cce5a511f8985eaf442.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Hader from "./components/header.vue";
+
 </script>
 
-
 <template>
-    <div class="header">
-    <Hader />
-</div>
-<div class="content">
-    <RouterView />
-</div>
+  
 </template>
 
-<style scoped lang="scss">
-.header{
-    width: 100vw;
-    height: 20vh;
-    background-color: #daa0a0;
-}
+<style scoped>
 
-.content{
-    width: 100vw;
-    height: 80vh;
-    
-}
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -13,21 +13,17 @@ export default{
 
 <template>
 <div class="headerShow">
-    <RouterLink to="/">首頁</RouterLink>
-    <RouterLink to="/Visit">觀光</RouterLink>
-    <RouterLink to="/Life">住宿</RouterLink>
-    <RouterLink to="/Transport">交通</RouterLink>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/"></RouterLink>
 
     
 </div>
-
 </template>
 
 <style scoped lang="scss">
-.headerShow{
-    background-color: rgb(195, 230, 254);
-}
-*{
-    font-size: 35px;
-}
+// .headerShow{
+//     width: 100%;
+//     height: 100%;
+//     background-color: aqua;
+// }
 </style>

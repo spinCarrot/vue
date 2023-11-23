fa30fb831285a8e53c2ebf9296aeae5aaaf6bd3c
Yuki00000000 <you@example.com>
2023-11-23 14:33:37 +0800

    header

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,10 +13,19 @@ export default{
 
 <template>
 <div class="headerShow">
-    <RouterLink to="/">首頁</RouterLink>
-    <RouterLink to="/Visit">觀光</RouterLink>
-    <RouterLink to="/Life">住宿</RouterLink>
-    <RouterLink to="/Transport">交通</RouterLink>
+    <div class="linkBtn home">
+        <RouterLink to="/" class="link">首頁</RouterLink>
+    </div>
+    <div class="linkBtn visit">
+        <RouterLink to="/Visit" class="link">觀光</RouterLink>
+    </div>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Emblem_of_Kaohsiung_City.svg/750px-Emblem_of_Kaohsiung_City.svg.png" alt="" class="logo">
+    <div class="linkBtn life">
+        <RouterLink to="/Life" class="link">住宿</RouterLink>
+    </div>
+    <div class="linkBtn transport">
+        <RouterLink to="/Transport" class="link">交通</RouterLink>
+    </div>
 
     
 </div>
@@ -26,8 +35,51 @@ export default{
 <style scoped lang="scss">
 .headerShow{
     background-color: rgb(195, 230, 254);
-}
-*{
-    font-size: 35px;
+    position: fixed;
+    position: relative;
+    padding-top: 15px;
+    
+    .logo{
+        width: 100px;
+        height: 120px;
+        position: absolute;
+        left: 46%;
+
+    }
+
+    .linkBtn{
+        width: 200px;
+        height: 80px;
+        background-color: azure;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 61px;
+        border-style:dashed ;
+        border-color:rgb(254,239,195);
+
+        .link{
+            text-decoration: none;
+            font-size: 35px;
+            font-weight: 700;
+        }
+    }
+    .home{
+        position: absolute;
+        left: 250px;
+    }
+    .visit{
+        position: absolute;
+        left: 450px;
+    }
+    .life{
+        position: absolute;
+        left: 850px;
+    }
+    .transport{
+        position: absolute;
+        left: 1050px;
+    }
 }
 </style>

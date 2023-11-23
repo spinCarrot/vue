aac0dd1a4581bd74885e3863c6bd25ecc67afd62
spinCarrot <h27732197@gmail.com>
2023-11-23 14:42:06 +0800

    ooo

--- src/stores/count.js
+++ src/stores/count.js
@@ -1,1 +1,27 @@
 import { defineStore } from 'pinia';
+
+export default defineStore("count", {
+    state() {
+        return {
+            gungon: [],
+            gginfo:[],
+            
+        }
+    },
+    getters: {
+        gunganName() {
+            return this.gungon
+        },
+        
+    },
+    actions:{
+        getGungon(){
+            fetch("https://api.kcg.gov.tw/api/service/Get/8ed53368-e292-4e2a-80a7-434cf497220c")
+            .then(res=>res.json())
+            .then(data=>{
+                this.gginfo = data;
+                console.log(this.gginfo)
+            })
+        }
+    }
+})
--- src/views/Life.vue
+++ src/views/Life.vue
@@ -1,10 +1,25 @@
 <script>
-import{ mapState, mapAction, defineStore} from 'pinia'
-import counter from '../stores/counter'
+import { mapState, mapActions } from 'pinia'
+import infor from '../stores/count'
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        ...mapState(infor, ["gungon", "gginfo"])
+    },
+    methods: {
+        ...mapActions(infor, ["getGungon"])
+    }
+}
 </script>
 
 <template>
-
+    <button type="button" @click="getGungon()">BTN</button>
+    <p>{{ this.gginfo }}</p>
 </template>
 
 <style scoped lang="scss"></style>

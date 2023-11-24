6eb34f7faa5b47e848fbd74ebc8aeed443af349b
m200m24 <m200isarifle@gmail.com>
2023-11-24 17:12:23 +0800

    觀光

--- src/stores/count.js
+++ src/stores/count.js
@@ -1,1 +1,33 @@
 import { defineStore } from 'pinia';
+
+
+export default defineStore("count", {
+    state() {
+        return {
+            add: [],
+            name:[],
+        }
+    },
+    getters: {
+        getadd() {
+            return this.add
+        }
+    },
+    actions: {
+        getadds() {
+            fetch("https://api.kcg.gov.tw/api/service/Get/82c544fa-ec50-4dea-8e8c-d8041abe8dee")
+                .then(res => res.json())
+                .then(data => {
+                    this.add = data;
+                    console.log(this.add.data);
+                    this.add.data.forEach(item=>{
+                        this.name.push(item.古蹟名稱)
+                    }),
+                    console.log(this.name);
+                })
+        }
+    }
+})
+
+
+

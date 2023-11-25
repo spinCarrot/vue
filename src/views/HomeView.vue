c486248e8e3e696c517280ac3b4885a3c22a6893
謝易樺 <a46644231@gmail.com>
2023-11-25 15:28:23 +0800

    template內容

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script>
-
+// import{ mapState, mapAction, defineStore} from 'pinia'
+// import counter from '../stores/counter'
 </script>
 
 <template>
-
+    <div class="top">
+        <h3>天氣預報</h3>
+        <select name="" id="city" v-model="cityText">
+            <option value="">請選擇</option>
+        </select>
+        <select name="" id="time">
+            <option value="">請選擇</option>
+        </select>
+        <div class="contentArea">
+            <div class="contentText contentItem">
+            <span id="cityName">{{ cityText }}</span>
+                <span id="condition"></span>
+                <div class="temp">
+                    <span id="minT"></span>
+                    <span>-</span>
+                    <span id="maxT"></span>
+                </div>
+                <div class="rain">
+                    <span>降雨機率：</span>
+                    <span id="rain"></span>
+                    <span>%</span>
+                </div>
+            </div>
+            <div class="contentImg contentItem">
+                <i class="fa-solid fa-rainbow"></i>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">

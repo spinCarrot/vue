755d249df84c570e11c7bb58b5e12ecb7cbe908b
spinCarrot <h27732197@gmail.com>
2023-11-27 16:53:42 +0800

    trytrysee

--- src/stores/count.js
+++ src/stores/count.js
@@ -4,12 +4,12 @@ export default defineStore("count", {
     state() {
         return {
             gungon: [],
-            ggadress:[],
-            ggname:[],
-            locat:[],
-            lugon:[],
-            minzu:[],
-            butto:"",
+            ggadress: [],
+            ggname: [],
+            locat: [],
+            lugon: [],
+            minzu: [],
+            butto: "",
         }
     },
     getters: {
@@ -32,52 +32,53 @@ export default defineStore("count", {
         //     return this.locat
         // }
 
-        
+
     },
-    actions:{
-        getGungon(a){
+    actions: {
+        getGungon(a) {
             // const btns = document.querySelectorAll("button[type='button']")
             fetch("https://api.kcg.gov.tw/api/service/Get/cb50902c-3681-43ec-89da-705f68eafb88")
-            .then(res=>res.json())
-            .then(obj=>{
-                
-                this.gungon = obj.data;
-                console.log(obj);
-                // console.log(this.gungon)
-                
-                
-                this.gungon.forEach(item =>{
-                    // this.butto=document.querySelectorAll("button[type='button']");
-                    // console.log(item)
-                    if(item.地址.includes(a)){
-                        this.ggname.push(item.觀光旅館名稱)
-                        console.log(item.觀光旅館名稱);
-                        console.log(ggname);
+                .then(res => res.json())
+                .then(obj => {
+
+                    this.gungon = obj.data;
+                    console.log(obj);
+                    // console.log(this.gungon)
+
+
+                    this.gungon.forEach(item => {
+                        
+                        if (item.地址.includes(a)) {
+                            this.ggname.push(item.觀光旅館名稱)
+                            console.log(item.觀光旅館名稱);
+                            console.log(this.ggname);
                         }
-                    // this.ggadress.push(item.地址)
+                        // this.ggadress.push(item.地址)
+                    })
+                    // this.gungon.data.forEach(item =>{
+                    //     this.ggname.push(item.觀光旅館名稱)
+                    // }),
+                    // console.log(this.ggadress)
+                    // console.log(this.ggname)
                 })
-                // this.gungon.data.forEach(item =>{
-                //     this.ggname.push(item.觀光旅館名稱)
-                // }),
-                // console.log(this.ggadress)
-                // console.log(this.ggname)
-            })
-        },
-        getLugon(){
             fetch("https://api.kcg.gov.tw/api/service/Get/8ed53368-e292-4e2a-80a7-434cf497220c")
-            .then(res=>res.json())
-            .then(obj1=>{
-                this.lugon=obj1,
-                console.log(this.lugon)
-            })
-        },
-        getMinzu(){
+                .then(res => res.json())
+                .then(obj1 => {
+                    this.lugon = obj1,
+                        console.log(this.lugon)
+                })
             fetch("https://api.kcg.gov.tw/api/service/Get/0696fe30-a7b7-42f7-9458-f4716138109d")
-            .then(res=>res.json())
-            .then(obj2=>{
-                this.minzu=obj2,
-                console.log(this.minzu)
-            })
+                .then(res => res.json())
+                .then(obj2 => {
+                    this.minzu = obj2,
+                        console.log(this.minzu)
+                })
+        },
+        getLugon() {
+
+        },
+        getMinzu() {
+
         }
     }
 })
--- src/views/Life.vue
+++ src/views/Life.vue
@@ -5,31 +5,59 @@ import infor from '../stores/count'
 export default {
     data() {
         return {
-            // const butto=document.querySelectorAll("button[type='button']")
-        butto:"",
+            tr: "",
+            bin: [],
         }
     },
     computed: {
-        ...mapState(infor, ["gungon", "ggadress", "ggname", "locat","lugon","minzu"])
+        ...mapState(infor, ["gungon", "ggadress", "ggname", "locat", "lugon", "minzu"])
     },
     methods: {
-        ...mapActions(infor, ["getGungon","getLugon","getMinzu"]),
-        pppp(){
-            const pop = document.getElementById("ccz")
-            this.getGungon(pop.value)
+        ...mapActions(infor, ["getGungon", "getLugon", "getMinzu"]),
+        pppp() {
+            // const pop = document.getElementById("ccz")
+            const btns = document.querySelectorAll("button[type='button']")
+            btns.forEach(item => {
+                item.addEventListener('click', () => {
+
+                    this.getGungon(item.value)
+                })
+            })
+            // this.getGungon(btns.value)
+        },
+        test() {
+            const id = document.getElementById("ccz")
+            console.log(this.bin)
+            this.bin.data.forEach((item, index) => {
+                if (item.鄉鎮 == id.value) {
+                    console.log(item)
+                }
+            })
         }
+    },
+    created() {
+        this.getGungon()
+        const body = document.querySelector("body")
+        body.addEventListener("mouseover",()=>{
+            if(this.bin.length == 0){
+                this.bin = this.lugon
+                console.log(this.bin)
+            }
+        
+            
+        })
     }
 }
 </script>
 
 <template>
-    
-    <button type="button" value="前鎮" @click="getGungon(a)" id="ccz">前鎮</button>
+    <!-- <p>{{ this.bin }}</p> -->
+    <button type="button" @click="test()">Btn</button>
+    <button type="button" value="前鎮" @click="this.pppp()" id="ccz">前鎮</button>
     <p>{{ this.ggname }}</p>
-    <button type="button" @click="getLugon()">鳳山</button>
-    <!-- <p>{{ this.lugon }}</p> -->
+    <button type="button" value="苓雅" @click="this.pppp(this.tr)">鳳山</button>
+    <p>{{ this.ggname }}</p>
     <button type="button" @click="getMinzu()">左營</button>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>

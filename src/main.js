48f36987abf45310dde141568ce6bec6bf33c3a8
spinCarrot <h27732197@gmail.com>
2023-11-27 09:13:11 +0800

    nosesabes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import './assets/main.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap'
+import './assets/main.scss'
+
 
 const app = createApp(App)
 const pinia = createPinia()
--- src/views/Life.vue
+++ src/views/Life.vue
@@ -5,21 +5,27 @@ import infor from '../stores/count'
 export default {
     data() {
         return {
-
+            // const butto=document.querySelectorAll("button[type='button']")
+        butto:"",
         }
     },
     computed: {
-        ...mapState(infor, ["gungon", "gginfo"])
+        ...mapState(infor, ["gungon", "ggadress", "ggname", "locat","lugon","minzu"])
     },
     methods: {
-        ...mapActions(infor, ["getGungon"])
+        ...mapActions(infor, ["getGungon","getLugon","getMinzu"])
+
     }
 }
 </script>
 
 <template>
-    <button type="button" @click="getGungon()">BTN</button>
-    <p>{{ this.gginfo }}</p>
+    
+    <button type="button" value="前鎮" @click="getGungon()">前鎮</button>
+    <p>{{ this.ggname }}</p>
+    <button type="button" @click="getLugon()">鳳山</button>
+    <!-- <p>{{ this.lugon }}</p> -->
+    <button type="button" @click="getMinzu()">左營</button>
 </template>
 
 <style scoped lang="scss"></style>

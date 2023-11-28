d5c5c442d5ebe6a0c056a421a15ef309a78de83e
謝易樺 <a46644231@gmail.com>
2023-11-28 12:27:21 +0800

    圖片說明

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -76,27 +76,59 @@ export default {
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/02.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>輕軌＆85大樓</h3>
+                        <p></p>
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/03.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>瑞豐夜市</h3>
+                        <p>擁有20年的歷史，高雄規模最大且管理最優的夜市</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/04.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>美濃紙傘</h3>
+                        <p>高雄市特有的客家文化象徵之一</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/05.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>旗津海灘</h3>
+                        <p>來旗津必訪之處，綿延數公里的弧形海岸線，細軟沙質、海水清澈</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/06.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>壽山動物園</h3>
+                        <p>台灣三大市立動物園之一，規模僅次於台北木柵動物園</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/07.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>義大世界</h3>
+                        <p>台灣首創古希臘情境主題遊樂園</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/08.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>佛光山</h3>
+                        <p>釋星雲法師創辦，台灣佛教四大名山之一</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
                     <img src="./圖片/09.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>鴨肉飯</h3>
+                        <p>網友推薦高雄最具代表性小吃</p>
+                    </div>
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -141,6 +173,9 @@ export default {
     <img src="./圖片/飛兒.png" alt="" class="img3">
     <img src="./圖片/小夕.png" alt="" class="img4">
 </div>
+<div class="bottom">
+    <p>外部連結：<a href="https://admin.khh.travel/">高雄市觀光局</a>、<a href="https://khh.travel/">高雄旅遊網</a>、<a href="https://www.krtc.com.tw/">高雄捷運全球資訊網</a>、<a href="https://www.welcometw.com/%E9%AB%98%E9%9B%84%E6%99%AF%E9%BB%9E%E6%8E%A8%E8%96%A6-%EF%BD%9C%E9%AB%98%E9%9B%84%E6%9C%80%E7%BE%8E%E6%9C%80%E5%A4%AFig%E6%89%93%E5%8D%A1%E6%99%AF%E9%BB%9E%EF%BC%8C%E5%B8%82%E5%8D%80%E3%80%81/">高雄必去景點推薦</a>、<a href="https://alerts.ncdr.nat.gov.tw/index.aspx?Mes=459169" >疫情示警</a></p>
+</div>
 </template>
 
 <style scoped lang="scss">
@@ -184,6 +219,16 @@ export default {
     width: 1550px;
     height: 679px;
     border-top: 10px solid purple;
+}
+
+.bottom{
+    width: 100vw;
+    height: 5vh;
+    background-color: darkgray;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 </style>
